9a9f135409e6c246a74c8151c51aba1dd5c980b5
S827 <44377607+S827@users.noreply.github.com>
2024-07-02 07:46:01 +0530

    added 2 more solutions of code validators

--- hackerank/stringValidators.py
+++ hackerank/stringValidators.py
@@ -54,4 +54,52 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     s = input()
 
+    for c in s:
+        if c.isalnum():
+            has_alnum = True
+        if c.isalpha():
+            has_alpha = True
+        if c.isdigit():
+            has_digit = True
+        if c.islower():
+            has_lower = True
+        if c.isupper():
+            has_upper = True
+
+    print(has_alnum)
+    print(has_alpha)
+    print(has_digit)
+    print(has_lower)
+    print(has_upper)
+
+# break from loop when already found the requirement
+
+if __name__ == "__main__":
+    s = input()
+    has_alnum = False
+    has_alpha = False
+    has_digit = False
+    has_lower = False
+    has_upper = False
+
+    for c in s:
+        if not has_alnum and c.isalnum():
+            has_alnum = True
+        if not has_alpha and c.isalpha():
+            has_alpha = True
+        if not has_digit and c.isdigit():
+            has_digit = True
+        if not has_lower and c.islower():
+            has_lower = True
+        if not has_upper and c.isupper():
+            has_upper = True
+
+        # if all conditions already met, break out from loop, as no need to iterate any more
+        if has_alnum and has_alpha and has_digit and has_lower and has_upper:
+            break
     
+    print(has_alnum)
+    print(has_alpha)
+    print(has_digit)
+    print(has_lower)
+    print(has_upper)
