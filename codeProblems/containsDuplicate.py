4415b2e92a4adebf609a4eb0d27959f3ed656f52
S827 <44377607+S827@users.noreply.github.com>
2024-07-08 23:17:56 +0530

    renamed folder and added containsDuplicate program

---
+++ codeProblems/containsDuplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
