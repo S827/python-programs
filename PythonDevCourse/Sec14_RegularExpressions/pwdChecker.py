9d9fe2941e37f89de4d658ea1fee9c4fa2533c8d
S827 <44377607+S827@users.noreply.github.com>
2024-09-07 00:19:02 +0530

    password validator

---
+++ PythonDevCourse/Sec14_RegularExpressions/pwdChecker.py
@@ -0,0 +1,12 @@
+# 8 char long
+# contain letters, numbers, symbols $%#@
+
+import re
+
+# pattern = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$%#@])[A-Za-z\d$%#@]{8,}$')
+pattern = re.compile(r'^[A-Za-z\d#$%@]{7,}\d$')
+
+string = 'ShaktiOP1@1'
+
+check = pattern.search(string)
+print(check)
