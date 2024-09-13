d4ab4d884cfc284801e6fe01694adf0082c2d96e
S827 <44377607+S827@users.noreply.github.com>
2024-09-13 09:04:36 +0530

    some changes

--- PythonDevCourse/Sec17_ScriptingInPython/WorkingWithEmails/customised_email_sender.py
+++ PythonDevCourse/Sec17_ScriptingInPython/WorkingWithEmails/customised_email_sender.py
@@ -2,17 +2,18 @@ import smtplib
 from email.message import EmailMessage
 from string import Template
 from pathlib import Path
+from config import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, TO_EMAIL, TO_EMAIL1  # type: ignore
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Eto Svn'
-email['to'] = 'shaktiv827@gmail.com'
+email['to'] = TO_EMAIL
 email['subject'] = 'Yo! You have won 1000000$'
 email.set_content(html.substitute({'name': 'Divya'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('nametest230@gmail.com', 'lvdc abhp hnny lxyo')
+    smtp.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     smtp.send_message(email)
     print('Email Sent')
--- PythonDevCourse/Sec17_ScriptingInPython/WorkingWithEmails/email_sender.py
+++ PythonDevCourse/Sec17_ScriptingInPython/WorkingWithEmails/email_sender.py
@@ -1,15 +1,16 @@
 import smtplib  # smtp : simple mail transfer protocol
 from email.message import EmailMessage
+from config import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, TO_EMAIL, TO_EMAIL1  # type: ignore
 
 email = EmailMessage()
 email['from'] = 'Eto Svn'
-email['to'] = 'shaktiv827@gmail.com'
+email['to'] = TO_EMAIL
 email['subject'] = 'Test Subject'
 
 email.set_content('This is test content')
 email1 = EmailMessage()
 email1['from'] = 'Eto Svn'
-email1['to'] = 'dskumar827@gmail.com'
+email1['to'] = TO_EMAIL1
 email1['subject'] = 'You have won 1000000$'
 email1.set_content('Enjoy your money. hahahahahahha!')
 
@@ -17,7 +18,7 @@ with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     print('hello')
     smtp.ehlo()
     smtp.starttls()  # tls: encryption mechanism
-    smtp.login('nametest230@gmail.com', 'lvdc abhp hnny lxyo')
+    smtp.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     smtp.send_message(email)
     smtp.send_message(email1)
     print('Done')
